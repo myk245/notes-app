d71c790e60594e2069acbb9a52901b483955ed7f
myk245 <myk245@nyu.edu>
2021-05-25 15:53:00 -0400

    add edit and delete buttons on each note

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // grab existing HTML elements
-const modal = document.getElementById("modal");
-const modalOpenButton = document.getElementById("add-button");
-const modalCloseButton = document.getElementById("close");
-const submitButton = document.getElementById("submit-button");
-const noteForm = document.getElementById("form");
-const noteInput = document.querySelector(".input");
+const modal = document.getElementById('modal');
+const modalOpenButton = document.getElementById('add-button');
+const modalCloseButton = document.getElementById('close');
+const submitButton = document.getElementById('submit-button');
+const noteForm = document.getElementById('form');
+const noteInput = document.querySelector('.input');
 const notesContainer = document.getElementById('notes-container');
 
 loadEventListeners = () => {
@@ -13,17 +13,17 @@ loadEventListeners = () => {
 
 // Event Listeners
 modalOpenButton.addEventListener('click', () => {
-   modal.style.display = "block";
+   modal.style.display = 'block';
 })
 
 modalCloseButton.addEventListener('click', () => {
-   modal.style.display = "none";
+   modal.style.display = 'none';
 }) 
    
 // if user clicks outside of modal, close it
 window.addEventListener('click', (event) => {
    if (event.target == modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
    }
 }) 
 
@@ -38,12 +38,22 @@ addNote = (event) => {
    note.className = 'note-object';
    note.appendChild(document.createTextNode(noteInput.value));
 
+   const deleteButton = document.createElement('button');
+   deleteButton.className = 'delete-button';
+   deleteButton.innerText = 'Delete';
+   note.appendChild(deleteButton);
+
+   const editButton = document.createElement('button');
+   editButton.className = 'edit-button';
+   editButton.innerText = 'Edit';
+   note.appendChild(editButton);
+
    notesContainer.appendChild(note);
    
    // clear notes field 
    noteInput.value = '';
    // close modal
-   modal.style.display = "none";
+   modal.style.display = 'none';
    
    console.log(noteInput.value)
    // alert("Note added!")
