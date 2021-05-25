31259faaf94b12a3d10600e563fb5c2585d11fb1
myk245 <myk245@nyu.edu>
2021-05-25 00:32:07 -0400

    begin implementing add note feature

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const modal = document.getElementById("modal");
 const modalOpenButton = document.getElementById("add-button");
 const modalCloseButton = document.getElementById("close");
+const submitButton = document.getElementById("submit-button");
+const noteInput = document.querySelector(".input");
+const noteForm = document.getElementById("form");
 
-modalOpenButton.onclick = function () {
+loadEventListeners = () => {
+   noteForm.addEventListener('submit', addNote);
+}
+
+// Event Listeners
+modalOpenButton.addEventListener('click', () => {
    modal.style.display = "block";
-}
+})
 
-modalCloseButton.onclick = function () {
+modalCloseButton.addEventListener('click', () => {
    modal.style.display = "none";
-}
-
+}) 
+   
 // if user clicks outside of modal, close it
-window.onclick = function (event) {
+window.addEventListener('click', (event) => {
    if (event.target == modal) {
       modal.style.display = "none";
    }
+}) 
+
+addNote = (event) => {
+   event.preventDefault();
+
+   if (noteInput.value === '') {
+      alert('You can\'t save an empty note.')
+   }
+
+   console.log(noteInput.value)
+   // alert("Note added!")
+
 }
+
+loadEventListeners();
